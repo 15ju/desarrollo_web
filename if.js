9303a94342e71f9ab4e637681411e7313edfa6c8
Julian Mendieta <julian.mendieta1501@gmail.com>
2023-01-21 18:32:03 -0300

    primeros de js

---
+++ if.js
@@ -0,0 +1,17 @@
+var edad = 18;
+
+if (edad >= 18) {
+    console.log("La persona es mayor de edad")
+} 
+else {
+    console.log("La persona es menor de edad")
+}
+    
+var Juana = {nombre:"juana", edad: 19}
+if (edad.Juana >= 18) {
+    console.log("La persona es mayor de edad")
+} 
+else {
+    console.log("La persona es menor de edad")
+}
+//iterador: es el ciclo for. Abro documento aparte
---
+++ variables.js
@@ -0,0 +1,20 @@
+/* Hay cinco tipos de datos en js:
+    logico
+    entero
+    coma flotante(decimales)
+    caracter
+    cadena(creo que habla del array)
+
+  Puedo usar developer mozilla para ver la teoria y todo lo referente a js
+
+  Variables con let o var las declaro
+*/
+
+let longitud = 16;
+let apellido = "Johnson";
+let x = {nombre:"john", apellido:"Doe"};
+
+console.log(x)
+//Si defino una variable que va a almacenar un numero no puedo sumarle un string, porque son tipos de datos diferentes
+//
+let a = 16 + "Volvo" 
