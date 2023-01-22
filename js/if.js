cdcace97f76b97ae5d3cbd85426bdf456633171c
Julian Mendieta <julian.mendieta1501@gmail.com>
2023-01-22 04:24:44 -0300

    primer js

--- js/if.js
+++ js/if.js
@@ -7,11 +7,3 @@ else {
     console.log("La persona es menor de edad")
 }
     
-var Juana = {nombre:"juana", edad: 19}
-if (edad.Juana >= 18) {
-    console.log("La persona es mayor de edad")
-} 
-else {
-    console.log("La persona es menor de edad")
-}
-//iterador: es el ciclo for. Abro documento aparte
---
+++ js/while.js
@@ -0,0 +1,8 @@
+var x = 10;
+
+while (x > 10) {
+    console.log("Mi variable vale " + x);
+    x--
+}
+
+// while (condicion) {Orden de hacer}
