086a67e331ce9c6fe71bf56f5425451c5db5c958
Julian Mendieta <julian.mendieta1501@gmail.com>
2023-01-27 18:59:49 -0300

    terminado

---
+++ html/js.js
@@ -0,0 +1,25 @@
+
+;
+
+/*function cambia() {
+    seleccionar = document.getElementById("refer").value;
+    selected()
+};*/
+/*
+function selected(seleccionar) {
+    var seleccionar = document.getElementById("refer").value;
+
+    if(seleccionar === '1') {
+        document.getElementById("refer1").innerHTML = "Ref: Andrea Ocampo, andrea.ocampo@segulatech.com"
+    } else { 
+        if(seleccionar === '0') {
+            document.getElementById("refer1").innerHTML = "Ref:"
+        }
+        else {
+            document.getElementById("refer1").innerHTML = "Ref: Juan Carlos Tesoro, jc.tesoro@ejemplo.com"
+        }
+    }
+    console.log(seleccionar)
+    var seleccionar = 0
+    console.log(seleccionar)
+};*/
